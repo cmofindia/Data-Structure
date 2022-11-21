5cc758a78c5fabc9db0748920c0c1c5b9b55c9a1
Chandramani Prasad <59479710+cmofindia@users.noreply.github.com>
2022-11-21 13:06:55 +0530

    List creation

--- selfRef.c
+++ selfRef.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct linked_list
 {
@@ -6,8 +7,53 @@ typedef struct linked_list
     struct linked_list *next;
 }NODE;
 
+NODE *create()
+{
+    NODE *newnode,*list,*root;
+    int num;
+    list=NULL;
+    root=NULL;
+    printf("\nEnter a number [0 to STOP]:");
+    scanf("%d",&num);
+    while(num!=0)
+    {
+        newnode=(NODE *)malloc(sizeof(NODE));
+        newnode->data=num;
+        newnode->next=NULL;
+        if(root==NULL)
+        {
+            root=newnode;
+        }
+        else 
+            list->next=newnode;
+
+        list=newnode;
+        printf("\nEnter a number [0 to STOP]:");
+        scanf("%d",&num);
+    }
+    return root;
+}
+
+void display(NODE *list)
+{
+    if(list)
+    {
+        while(list)
+        {
+            printf("%5d",list->data);
+            list=list->next;
+        }
+    }
+    else 
+    {
+        printf("\nList is empty");
+    }
+}
 int main()
 {
-    printf("Creating Linked list");
+    printf("\nCreating Linked list\n");
+    NODE *first=create();
+    printf("\nCreated Linked list\n");
+    display(first);
     return 0;
 }
